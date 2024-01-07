d03ddc30d2334994d364c0d7879010dc5d07d8d8
Dhaval Vegad <96942545+dhavalvegad111@users.noreply.github.com>
2024-01-07 14:32:15 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,11 +1,13 @@
 <?php
-include "db_conn.php";
+include "connection.php";
+
 $id = $_GET["id"];
 $sql = "DELETE FROM `crud` WHERE id = $id";
 $result = mysqli_query($conn, $sql);
 
 if ($result) {
-  header("Location: index.php?msg=Data deleted successfully");
+  header("Location: index.php?msg=Data deleted successfully.");
 } else {
   echo "Failed: " . mysqli_error($conn);
 }
+?>

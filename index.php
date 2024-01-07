40a05627387550cf00f8adfc2365b69fa5f277f5
Dhaval Vegad <96942545+dhavalvegad111@users.noreply.github.com>
2024-01-07 14:31:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,50 +1,52 @@
 <?php
-include "db_conn.php";
+  include "connection.php";
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
+  <!-- DataTables -->
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.8/css/dataTables.bootstrap5.min.css" />
   <title>PHP CRUD Application</title>
 </head>
-
 <body>
   <nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color: #00ff5573;">
-    PHP Complete CRUD Application
+  <a href="index.php" class="text-decoration-none text-black">PHP Complete CRUD Application</a>
   </nav>
-
   <div class="container">
+    <div class="msg">
     <?php
     if (isset($_GET["msg"])) {
       $msg = $_GET["msg"];
-      echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+      echo '<div class="msgBox alert alert-warning alert-dismissible fw-bold fade show" role="alert">
       ' . $msg . '
       <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>';
     }
-    ?>
-    <a href="add-new.php" class="btn btn-dark mb-3">Add New</a>
-
-    <table class="table table-hover text-center">
-      <thead class="table-dark">
+    ?> 
+    </div>
+  </div>
+  <div class="container">
+  <div class="main border border-gray rounded-3 p-4">
+    <a href="insert.php" class="btn btn-dark mb-3">Add New</a>
+    <table class="table table-striped table-hover w-100" id="dataTable">
+      <thead class="">
         <tr>
-          <th scope="col">ID</th>
-          <th scope="col">First Name</th>
-          <th scope="col">Last Name</th>
-          <th scope="col">Email</th>
-          <th scope="col">Gender</th>
-          <th scope="col">Action</th>
+          <th>ID</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>Gender</th>
+          <th>Created At</th>
+          <th>Updated At</th>
+          <th>Actions</th>
         </tr>
       </thead>
       <tbody>
@@ -58,10 +60,13 @@ include "db_conn.php";
             <td><?php echo $row["first_name"] ?></td>
             <td><?php echo $row["last_name"] ?></td>
             <td><?php echo $row["email"] ?></td>
+            <td><?php echo $row["phone"] ?></td>
             <td><?php echo $row["gender"] ?></td>
+            <td><?php echo $row["created_at"] ?></td>
+            <td><?php echo $row["updated_at"] ?></td>
             <td>
-              <a href="edit.php?id=<?php echo $row["id"] ?>" class="link-dark"><i class="fa-solid fa-pen-to-square fs-5 me-3"></i></a>
-              <a href="delete.php?id=<?php echo $row["id"] ?>" class="link-dark"><i class="fa-solid fa-trash fs-5"></i></a>
+              <a href="update.php?id=<?php echo $row["id"] ?>" class="link-dark" title="Edit"><i class="fa-solid fa-pen-to-square fs-5 me-3"></i></a>
+              <a href="delete.php?id=<?php echo $row["id"] ?>" onclick="return confirm('Are you sure you want to delete?')" class="link-dark" title="Delete"><i class="fa-solid fa-trash fs-5"></i></a>
             </td>
           </tr>
         <?php
@@ -70,10 +75,23 @@ include "db_conn.php";
       </tbody>
     </table>
   </div>
-
+  </div>
+  <!-- jQuery -->
+  <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
   <!-- Bootstrap -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-
+  <!-- DataTables -->
+  <script src="https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.13.8/js/dataTables.bootstrap5.min.js"></script>
+  <script>
+    $(document).ready(function(){
+      // DataTable
+      new DataTable('#dataTable');
+      // msgBox fade out
+      setTimeout(function() {
+        $('.msg').fadeOut(500);        
+      }, 2000);
+    });
+  </script>
 </body>
-
 </html>
